08afef6e7a8e582a185007faa41b858f51f4fc83
Helen Johnson <johnsonhelen31@gmail.com>
2017-07-13 21:07:47 +0100

    Add Tests

--- test/user_test.js
+++ test/user_test.js
@@ -1,7 +1,12 @@
+import app from '../app.js';
+
 const should = require('chai').should(),
-  expect = require('chai').expect,
-  supertest = require('supertest'),
-  api = supertest('localhost:8000');
+expect = require('chai').expect,
+supertest = require('supertest');
+
+const api = supertest('localhost:8000');
+
+const requestHandler = supertest(app);
 
 describe('user', function () {
   it('should return a 200 response', function (done) {
@@ -10,20 +15,27 @@ describe('user', function () {
       .expect(200, done);
   });
 
-  it('should be an object with keys and values', function (done) {
-    api.post('/api/users/signup')
-      .set('Accept', 'application/json')
-      .expect(201)
-      .end(function(err, res) {
-        expect(res.body).to.have.property('username');
-        expect(res.body.username).to.not.equal(null);
-        expect(res.body).to.have.property('password');
-        expect(res.body.password).to.not.equal(null);
-        expect(res.body).to.have.property('email');
-        expect(res.body.email).to.not.equal(null);
-        expect(res.body).to.have.property('phonenumber');
-        expect(res.body.phonenumber).to.not.equal(null);
-        done();
-      });
+  const testUser = {
+    username: 'Nelson',
+    password: 'chinels',
+    email: 'testme@yahoo.ng',
+    phonenumber: '01245622',
+  };
+
+  let token = '';
+
+  describe('User', () => {
+    it('should create new user', (done) => {
+      requestHandler.post('/api/users/signup')
+        .set('Accept', 'application/json')
+        .send(testUser)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.status).to.equal(testUser.username);
+          token = res.body.data.token;
+          done();
+        });
+    });
   });
 });
+
